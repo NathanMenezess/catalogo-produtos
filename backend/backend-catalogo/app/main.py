8caa34ece01aacbb7c13c901d3b158ba6dd59177
Nathan <menezesnathan61@gmail.com>
2025-12-14 03:06:41 -0300

    Restringe CORS para frontend

--- backend/backend-catalogo/app/main.py
+++ backend/backend-catalogo/app/main.py
@@ -17,7 +17,10 @@ app = FastAPI()
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # TEMPORÁRIO — depois fechamos para o domínio do Vercel
+        allow_origins=[
+        "http://localhost:5173",
+        "https://catalogo-front.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
