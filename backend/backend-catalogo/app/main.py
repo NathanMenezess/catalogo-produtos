ac5b3fe5849a9bd6e902b41b8416bf83a14cfbea
Nathan <menezesnathan61@gmail.com>
2026-02-03 17:38:18 -0300

    Arrumando bugs

--- backend/backend-catalogo/app/main.py
+++ backend/backend-catalogo/app/main.py
@@ -19,10 +19,7 @@ app = FastAPI()
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://catalogo-front.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
